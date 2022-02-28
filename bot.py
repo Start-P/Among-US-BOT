166e65d5ae2a81e2b60e03b4ff0ea8cd1e2fb924
Start-P <92899739+Start-P@users.noreply.github.com>
2022-02-28 21:00:28 +0900

    Music Player

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-
+import ffmpeg #replitで実行するため、オートインポートに引っかかるようにする。 import os -> os.system("pip install ffmpeg") でも良い
 client = discord.Client()
 @client.event
 async def on_message(msg):
@@ -11,4 +11,8 @@ async def on_message(msg):
     for i in msg.author.voice.channel.members:
       await i.edit(mute=False)
     await msg.channel.send("Success Unmute!")
+  if msg.content.startwith("!play"):
+    voice = await msg.author.voice.channel.connect() #vcに接続
+    music = await voice.create_ytdl_player(msg.content.split(" ")[1]) #空白でsplitしてurlを抽出
+    music.start()
 client.run(os.environ["TOKEN"])
